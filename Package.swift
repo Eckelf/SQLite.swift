547a64f843cc3edd7467bc1ff9b6b215d87df81e
Vincent Flecke <git@eckelf.net>
2020-11-28 22:09:14 +0100

    Link with SQLite system library

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ let package = Package(
     name: "SQLite.swift",
     products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
-        .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
+        .systemLibrary(
+            name: "CSQLite",
+            providers: [.apt(["libsqlite3-dev"])]),
+        .target(name: "SQLite", dependencies: ["SQLiteObjc", "CSQLite"]),
         .target(name: "SQLiteObjc"),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
     ],
